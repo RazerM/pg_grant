23e3d12b66ccf58de4b89fb23e30a12c96ba7077
Frazer McLean <frazer@frazermclean.co.uk>
2017-10-08 22:12:13 +0200

    Add test-schema.sql

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 import pytest
 from sqlalchemy import create_engine
 from testcontainers import PostgresContainer as _PostgresContainer
+
+tests_dir = Path(__file__).parents[0].resolve()
+test_schema_file = Path(tests_dir, 'data', 'test-schema.sql')
 
 
 class PostgresContainer(_PostgresContainer):
@@ -10,7 +15,7 @@ class PostgresContainer(_PostgresContainer):
 
 @pytest.fixture(scope='session')
 def postgres_url():
-    postgres_container = PostgresContainer("postgres:10")
+    postgres_container = PostgresContainer("postgres:latest")
     with postgres_container as postgres:
         yield postgres.get_connection_url()
 
@@ -22,7 +27,7 @@ def engine(postgres_url):
 
 @pytest.fixture(scope='session')
 def schema(engine):
-    with open('schema.sql') as fp:
+    with test_schema_file.open() as fp:
         engine.execute(fp.read())
 
 
