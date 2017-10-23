5865da0191d5660feae8312c826a53fa23b8a7ba
Frazer McLean <frazer@frazermclean.co.uk>
2017-10-24 01:30:18 +0200

    Bump version: 0.2.0 → 0.2.0.post0

--- pg_grant/__init__.py
+++ pg_grant/__init__.py
@@ -3,7 +3,7 @@ from .parse import get_default_privileges, parse_acl_item
 from .types import (
     FunctionInfo, PgObjectType, Privileges, RelationInfo, SchemaRelationInfo)
 
-__version__ = '0.2.0'
+__version__ = '0.2.0.post0'
 __description__ = 'Parse PostgreSQL privileges'
 
 __license__ = 'MIT'
